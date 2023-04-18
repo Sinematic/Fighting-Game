3c7672cbf26cce78759b3f871424acdabc0e3411
Maxime <maxime.rache@gmail.com>
2023-04-18 19:05:23 +0200

    Ajout limitations des déplacements au cadre de jeu

--- js/game.js
+++ js/game.js
@@ -6,22 +6,44 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.3
+const gravity = 0.4
+
+const keys = {
+    d: {
+        pressed: false
+    },
+    q: {
+        pressed: false
+    },
+    z: {
+        pressed: false
+    }
+}
+
 
 class Sprite {
 
-    constructor({position, velocity}) {
-
-        this.position = position,
+    constructor({position, velocity, color = "red"}) {
+        this.position = position
         this.velocity = velocity
         this.height = 150
         this.width = 50
         this.lastKey
+        this.color = color
+        this.isAttacking = false
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        }
     }
 
     draw() {
-        c.fillStyle = "red"
+        c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        c.fillStyle = "green"
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
     }
 
     update() {
@@ -32,6 +54,20 @@ class Sprite {
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
         } else this.velocity.y += gravity
+ 
+        if (this.position.x < 0) {
+            this.position.x = 0
+            this.velocity.x = 0
+        }
+
+        if (this.position.x + this.width > canvas.width) {
+            this.position.x = canvas.width - this.width
+            this.velocity.x = 0
+        }
+    }
+
+    attack() {
+        this.isAttacking = true
     }
 }
 
@@ -43,12 +79,11 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 10
-    }
+    },
+    color: "blue"
 })
 
-console.log(player)
 player.draw()
-
 
 const enemy = new Sprite({
     position: {
@@ -63,12 +98,24 @@ const enemy = new Sprite({
 
 enemy.draw()
 
+console.log(player)
+
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = "black"
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+
+    if (keys.d.pressed && player.lastKey === "d") {
+        player.velocity.x = 1
+    } else if (keys.q.pressed && player.lastKey === "q") {
+        player.velocity.x = -1
+    } else if (keys.z.pressed && player.lastKey === "z") {
+    }
 }
 
 animate()
@@ -78,13 +125,16 @@ document.addEventListener("keydown", (event) => {
     switch(event.key) {
 
         case "d":
-            player.velocity.x = 1
+            keys.d.pressed = true
+            player.lastKey = "d"
             break
-        case "f":
-            player.velocity.x = -1
-        break
-        default:
-            console.log(event.key)
+        case "q":
+            keys.q.pressed = true
+            player.lastKey = "q"
+            break
+        case "z":
+            player.velocity.y = -15
+            break
     }
 })
 
@@ -93,12 +143,13 @@ document.addEventListener("keyup", (event) => {
     switch(event.key) {
 
         case "d":
-            player.velocity.x = 0
+            keys.d.pressed = false
             break
-        case "f":
-            player.velocity.x = 0
-        break
-        default:
-            console.log(event.key)
+        case "q":
+            keys.q.pressed = false
+            break
+        case "z":
+            player.velocity.y = 0
     }
 })
+
